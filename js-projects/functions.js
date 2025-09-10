2ff18d3243ad00ce5a50aedb1c3669dc2adefa17
Maltiti Azindoo Alidu <153810482+alidumaltiti@users.noreply.github.com>
2025-09-10 09:57:55 +0000

    Add JavaScript introduction examples and light bulb demo
    Added a light bulb demo with HTML and images to demonstrate DOM manipulation. Updated index.html with JavaScript examples and improved structure. Added new js-projects directory with basic HTML and JavaScript function examples.

---
+++ js-projects/functions.js
@@ -0,0 +1,25 @@
+function greet(name) {
+    return 'Hello, Maltiti';
+}
+
+console.log(greet("Eddy"));
+
+const add = function(a, b) {
+    return a + b;
+};
+console.log(add(3, 4));
+
+function greet(name) {
+    return 'Hello, ${name}!';
+}
+
+console.log(greet("Eddy"));
+
+let x =10, y = 20;
+console.log (x + y)
+
+
+let a=20, b=40, c=10;
+console.log(a + b)
+console.log(a*b)
+console.log(a+b/c)
